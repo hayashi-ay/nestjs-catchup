fc7826e9d474d4aa746aeaa9482fdecc7285ec15
Ayato Hayashi <ayato.hayashi@justincase-tech.com>
2022-09-07 11:45:42 +0900

    fix: mockデータではなくMongoDBを使うように

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,23 +1,20 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Coffee } from './entities/conffee.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
 export class CoffeesService {
-	private coffees: Coffee[] = [
-		{
-			id: 1,
-			name: 'Shipwreck Roast',
-			brand: 'Buddy Brew',
-			flavors: ['chocolate', 'vanilla']
-		}
-	]
+	constructor(
+		@InjectModel(Coffee.name) private readonly coffeeModel: Model<Coffee>,
+	) {}
 
 	findAll() {
-		return this.coffees;
+		return this.coffeeModel.find().exec();
 	}
 
-	findOne(id: string) {
-		const coffee = this.coffees.find(item => item.id === +id)
+	async findOne(id: string) {
+		const coffee = await this.coffeeModel.findOne({ _id: id}).exec();
 		if (!coffee) {
 			throw new NotFoundException(`Coffee #${id} not found.`);
 		}
@@ -25,20 +22,23 @@ export class CoffeesService {
 	}
 
 	create(createCoffeeDto: any) {
-		this.coffees.push(createCoffeeDto);
+		const coffee = new this.coffeeModel(createCoffeeDto);
+		return coffee.save();
 	}
 
-	update(id: string, updateCoffeeDto: any) {
-		const existingCoffee = this.findOne(id);
-		if (existingCoffee) {
-			// update
+	async update(id: string, updateCoffeeDto: any) {
+		const existingCoffee = this.coffeeModel
+			.findOneAndUpdate({ _id: id }, { $set: updateCoffeeDto }, { new: true})
+			.exec();
+		
+		if (!existingCoffee) {
+			throw new NotFoundException(`Coffee #${id} not found.`);
 		}
+		return existingCoffee;
 	}
 
-	remove(id: string) {
-		const coffeeIndex = this.coffees.findIndex(item => item.id === +id);
-		if (coffeeIndex >= 0) {
-			this.coffees.splice(coffeeIndex, 1);
-		}
+	async remove(id: string) {
+		const coffee = await this.findOne(id);
+		return coffee.remove();
 	}
 }
